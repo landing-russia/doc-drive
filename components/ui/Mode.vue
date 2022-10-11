c8c787cfcae11dc12c0b90a67d0a6c3d96e13dd7
landing-russia <landing-russia@yandex.ru>
2022-10-11 09:27:44 +0300

    upgrade ncu -u

--- components/ui/Mode.vue
+++ components/ui/Mode.vue
@@ -14,7 +14,7 @@ const setColorTheme = (newTheme: Theme) => {
     <svg
       v-show="$colorMode.value === 'dark'"
       xmlns="http://www.w3.org/2000/svg"
-      class="h-5 w-5 text-slate-500 hover:text-slate-400"
+      class="h-5 w-5 text-slate-400 hover:text-slate-200"
       fill="none"
       viewBox="0 0 24 24"
       stroke="currentColor"
